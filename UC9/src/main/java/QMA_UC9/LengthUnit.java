99bcfaa0289da6ffbf54e3b10e67146be5f03208
navyaa274 <navu.aggarwal@gmail.com>
2026-03-20 00:50:32 +0530

    UC9: Weight Measurement Equality, Conversion, and Addition (Kilogram, Gram, Pound)

---
+++ UC9/src/main/java/QMA_UC9/LengthUnit.java
@@ -0,0 +1,26 @@
+package QMA_UC9;
+
+public enum LengthUnit {
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    double convertToBaseUnit(double value) {
+        return value * this.conversionFactor;
+    }
+
+    static double convertFromBaseToTargetUnit(double baseValue, LengthUnit targetUnit) {
+        return baseValue / targetUnit.conversionFactor;
+    }
+}
