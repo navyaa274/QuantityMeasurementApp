5bfef8dfb36a4265106f6a7c193da4545c84b02f
navyaa274 <navu.aggarwal@gmail.com>
2026-04-02 07:02:27 +0530

    UC13: Centralized Arithmetic Logic to Enforce DRY in Quantity Operations

---
+++ UC14/src/main/java/IMeasurable.java
@@ -0,0 +1,12 @@
+package QMA_UC13;
+
+public interface IMeasurable {
+    public double getConversionFactor();
+    public double convertToBaseUnit(double value);
+    public double convertFromBaseUnit(double baseValue);
+    public String getUnitName();
+
+    public static void main(String[] args) {
+        System.out.println("QMA_UC13.IMeasurable Interface");
+    }
+}
